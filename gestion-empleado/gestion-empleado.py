68dfb2771e6bac1e5cc953f537f575f7d55b2f64
jheral-andres <jheral.proyectos@gmail.com>
2023-05-18 16:18:30 -0400

    Estructura MVC, codificando la interfaz de inicio de sesion y registrase

---
+++ gestion-empleado/gestion-empleado.py
@@ -0,0 +1,9 @@
+from client.interfaz import Ventana
+
+def main():
+    
+    app = Ventana()
+    app.mainloop()
+    
+if __name__ == '__main__':
+    main()
---
+++ gestion-empleado/model/conexion.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+class Conexion():
+    def __init__(self):
+        pass
+
+    def creacion_tablas(self):
+        pass
+
+    
