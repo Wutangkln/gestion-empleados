da599c0b6eb216f2b143c6825653aa84fc4f5d93
jheral-andres <jheral.proyectos@gmail.com>
2023-06-20 21:43:49 -0400

    Actualizacion de creacion de tablas en la base de datos

--- gestion-empleado/model/conexion.py
+++ gestion-empleado/model/conexion.py
@@ -26,8 +26,8 @@ class Conexion():
             "CREATE TABLE empleado_contacto (id_empleado_contacto INT PRIMARY KEY AUTO_INCREMENT, id_empleado INT, id_contacto_emergencia INT, FOREIGN KEY (id_empleado) REFERENCES empleados(id_empleado), FOREIGN KEY (id_contacto_emergencia) REFERENCES contactos_emergencia(id_contacto_emergencia));",
             "CREATE TABLE informacion_laboral (id_informacion_laboral INT PRIMARY KEY AUTO_INCREMENT, fecha_ingreso DATE, id_empleado INT, FOREIGN KEY (id_empleado) REFERENCES empleados(id_empleado));",
             "CREATE TABLE departamento (id_departamento INT PRIMARY KEY AUTO_INCREMENT, nombre_departamento VARCHAR(100), id_informacion_laboral INT, FOREIGN KEY(id_informacion_laboral) REFERENCES informacion_laboral(id_informacion_laboral));",
-            "CREATE TABLE area (id_area INT PRIMARY KEY AUTO_INCREMENT, nombre_area VARCHAR(100), id_departamento INT, FOREIGN KEY(id_departamento) REFERENCES departamento(id_departamento));",
-            "CREATE TABLE cargo (id_cargo INT PRIMARY KEY AUTO_INCREMENT, nombre_cargo VARCHAR(100), id_area INT, FOREIGN KEY(id_area) REFERENCES area(id_area));",
+            "CREATE TABLE area (id_area INT PRIMARY KEY AUTO_INCREMENT, nombre_area VARCHAR(100), id_departamento INT, id_empleado INT, FOREIGN KEY(id_departamento) REFERENCES departamento(id_departamento), FOREIGN KEY (id_empleado) REFERENCES empleados(id_empleado));",
+            "CREATE TABLE cargo (id_cargo INT PRIMARY KEY AUTO_INCREMENT, nombre_cargo VARCHAR(100), id_area INT, id_empleado INT, FOREIGN KEY(id_area) REFERENCES area(id_area), FOREIGN KEY (id_empleado) REFERENCES empleados(id_empleado));",
             "CREATE TABLE cargas_familiares (id_carga_familiar INT PRIMARY KEY AUTO_INCREMENT, nombre VARCHAR(100), rut VARCHAR(20), sexo VARCHAR(20), parentesco VARCHAR(100), id_empleado INT, FOREIGN KEY (id_empleado) REFERENCES empleados(id_empleado));",
             "CREATE TABLE perfil (id_perfil INT PRIMARY KEY AUTO_INCREMENT, nombre_usuario VARCHAR(100), clave VARCHAR(100), tipo_perfil VARCHAR(100), id_empleado INT, FOREIGN KEY (id_empleado) REFERENCES empleados(id_empleado));"
         ]
